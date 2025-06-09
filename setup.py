24683044884cda11f041d755589674f3e8ce3e76
avizaguri-macbook <avi210892@gmail.com>
2025-06-09 17:04:13 +0300

    Rename elastic_search module to elasticsearch and update import paths

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '0.1.0'
+package_version = '0.1.1'
 
 package_name = 'python-databases'
 package_description = 'A Python library for managing and interacting with multiple databases seamlessly.'
