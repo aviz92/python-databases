7a436c0d194679071862db1e188146b06625d408
avizaguri-macbook <avi210892@gmail.com>
2025-09-13 20:13:05 +0300

    Bump package version to 0.1.4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '0.1.3'
+package_version = '0.1.4'
 
 package_name = 'python-databases'
 package_description = 'A Python library for managing and interacting with multiple databases seamlessly.'
